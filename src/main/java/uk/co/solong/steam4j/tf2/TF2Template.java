03136064638315a33e0a74a5c2f56d1fa655e8c2
danielburrell <daniel.burrell@gmail.com>
2014-12-21 04:23:18 +0000

    line endings again
    line endings again

--- src/main/java/uk/co/solong/steam4j/tf2/TF2Template.java
+++ src/main/java/uk/co/solong/steam4j/tf2/TF2Template.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
